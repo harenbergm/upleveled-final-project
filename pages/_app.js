7cae1995598f287cc28f840abfdb666899d27dcd
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-10-27 21:01:56 +0200

    Add emotion and golbal styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { css, Global } from '@emotion/react';
+
+// import { css } from '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global // global styles
+        styles={css`
+          *,
+          *::before,
+          *::after {
+            text-decoration: none;
+            box-sizing: border-box;
+          }
+
+          body {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+            margin: 0px;
+          }
+        `}
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
