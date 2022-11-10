1340d6e355ff462b071c89801eccaeb5dd8544b9
Michael Harenberg <michaelharenberg.bsc@gmail.com>
2022-11-10 23:20:18 +0100

    added styling to profile page

--- pages/private-profile.tsx
+++ pages/private-profile.tsx
@@ -1,8 +1,8 @@
+import { css } from '@emotion/react';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-// import { userAgent } from 'next/server';
 import { useState } from 'react';
-import RecipeUpload from '../components/RecipeUpload';
+// import RecipeUpload from '../components/RecipeUpload';
 import UploadImage from '../components/UploadImage';
 import getIngredients from '../database/ingredients';
 import { getUserBySessionToken, User } from '../database/users';
@@ -13,10 +13,89 @@ type Props = {
 };
 
 export default function UserProfile(props: Props) {
+  const profileStyles = css`
+    margin: 50px 200px 0px;
+
+    span {
+      display: block;
+      margin-bottom: 20px;
+    }
+  `;
+
+  const personalInformationStyles = css`
+    label {
+      display: inline-block;
+      border-radius: 4px;
+      margin-bottom: 10px;
+      font-weight: 500;
+    }
+
+    input {
+      margin-left: 50px;
+      margin-right: 30px;
+    }
+
+    button {
+      border: 2px solid green;
+      display: inline-block;
+      border-radius: 4px;
+      background-color: white;
+      color: green;
+    }
+    #deleteProfile {
+      display: block;
+      margin-top: 20px;
+      border: red 1px solid;
+      border-radius: 4px;
+      background-color: white;
+      color: red;
+      margin-bottom: 30px;
+    }
+  `;
+
+  const recipeStyles = css`
+    h4 {
+      margin-top: 40px;
+    }
+    #ingredients {
+      margin-right: 10px;
+      margin-bottom: 10px;
+    }
+    #instructions {
+      width: 100%;
+      height: 300px;
+    }
+    button {
+      margin-top: 20px;
+      border: 2px solid green;
+      display: inline-block;
+      border-radius: 4px;
+      background-color: white;
+      color: green;
+    }
+  `;
+
   const [username, setUsername] = useState(props.user.username);
   const [email, setEmail] = useState(props.user.eMail);
   const [imageUrl, setImageUrl] = useState('');
+  const [recipeTitle, setRecipeTitle] = useState('');
+  const [recipeInstructions, setRecipeInstructions] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  console.log('ingredients', ingredients);
 
+  // selects and filter the ingredients
+  function handleCheck(index: number) {
+    if (ingredients.includes(index)) {
+      const filteredIngredient = ingredients.filter((ingredient) => {
+        return ingredient !== index;
+      });
+      setIngredients(filteredIngredient);
+    } else {
+      setIngredients([...ingredients, index]);
+    }
+  }
+
+  // creates recipe
   async function createRecipeFromApi() {
     const response = await fetch(`/api/recipes`, {
       method: 'POST',
@@ -25,6 +104,7 @@ export default function UserProfile(props: Props) {
       },
       body: JSON.stringify({
         imageURL: imageUrl,
+        ingredientsSelected: ingredients,
       }),
     });
 
@@ -32,6 +112,7 @@ export default function UserProfile(props: Props) {
     console.log('cloudinaryBodyUrl', cloudinaryBodyUrl);
   }
 
+  // Updates User Profile
   async function updateUserFromApiById(id: number) {
     const response = await fetch(`/api/profiles/${id}`, {
       method: 'PUT',
@@ -60,17 +141,17 @@ export default function UserProfile(props: Props) {
     });
   }
 
-  async function createRecipeFromApiById(id: number) {
-    const response = await fetch(`/api/profiles/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify({
-        iD: id,
-      }),
-    });
-  }
+  // async function createRecipeFromApiById(id: number) {
+  //   const response = await fetch(`/api/profiles/${id}`, {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'content-type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       iD: id,
+  //     }),
+  //   });
+  // }
 
   if (!props.user) {
     return (
@@ -90,70 +171,120 @@ export default function UserProfile(props: Props) {
         <title>Personal Information</title>
         <meta name="description" content="Biography of the person" />
       </Head>
-      <h1>Private Profile</h1>
-      Account ID: {props.user.id}
-      <br />
-      <h2>Personal Information</h2>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-        }}
-      >
-        <label>
-          Username:
-          <input
-            value={username}
-            onChange={(event) => {
-              setUsername(event?.currentTarget.value);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          E-Mail:
-          <input
-            value={email}
-            onChange={(event) => {
-              setEmail(event?.currentTarget.value);
-            }}
-          />
-          <br />
-          <br />
-          <button
-            onClick={() => {
-              updateUserFromApiById(props.user.id);
-            }}
-          >
-            Save Changes
-          </button>
-        </label>
-      </form>
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          deleteUserFromApiById(props.user.id);
-        }}
-      >
-        Delete Profile
-      </button>
-      <br />
-      <br />
-      <h2>Create Recipe</h2>
-      <div>
-        <h4>Upload Image</h4>
-        <form>
-          <UploadImage setImageUrl={setImageUrl} />
-          <button
-            onClick={(event) => {
-              createRecipeFromApi();
-              event.preventDefault();
-            }}
-          >
-            Submit
-          </button>
-          <RecipeUpload ingredients={props.ingredients} />
+      <div css={profileStyles}>
+        <h1>Private Profile</h1>
+        <hr />
+        <h2>Personal Information</h2>
+        <span> Account ID: {props.user.id}</span>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <div css={personalInformationStyles}>
+            <label>
+              Username:
+              <input
+                value={username}
+                onChange={(event) => {
+                  setUsername(event?.currentTarget.value);
+                }}
+              />
+            </label>
+            <button
+              onClick={() => {
+                updateUserFromApiById(props.user.id);
+              }}
+            >
+              Save Changes
+            </button>
+            <br />
+            <label>
+              E-Mail:
+              <input
+                value={email}
+                onChange={(event) => {
+                  setEmail(event?.currentTarget.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  updateUserFromApiById(props.user.id);
+                }}
+              >
+                Save Changes
+              </button>
+            </label>
+            <button
+              id="deleteProfile"
+              onClick={() => {
+                deleteUserFromApiById(props.user.id);
+              }}
+            >
+              Delete Profile
+            </button>
+            <hr />
+          </div>
         </form>
+        <div css={recipeStyles}>
+          <h2>Share Your Recipes</h2>
+          <p>
+            Share your recipes in four easy steps and get in touch with
+            like-minded cooking lovers!
+          </p>
+          <div>
+            <form>
+              <h4>1. Upload Image</h4>
+              <UploadImage setImageUrl={setImageUrl} />
+              <div>
+                <h4>2. Chose a Title</h4>
+
+                <input
+                  value={recipeTitle}
+                  onChange={(event) => {
+                    setRecipeTitle(event?.currentTarget.value);
+                  }}
+                />
+
+                <h4>3. Chose Ingredients</h4>
+                {props.ingredients.map((ingredient: any, index: any) => {
+                  return (
+                    <label key={index}>
+                      {ingredient.name}
+                      <input
+                        id="ingredients"
+                        value={ingredient.id}
+                        type="checkbox"
+                        onChange={() => {
+                          handleCheck(index + 1);
+                        }}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
+              <div>
+                <h4>Instruction (max. 1000 chars)</h4>
+                <input
+                  id="instructions"
+                  value={recipeInstructions}
+                  onChange={(event) => {
+                    setRecipeInstructions(event.currentTarget.value);
+                  }}
+                />
+              </div>
+              {/* <RecipeUpload ingredients={props.ingredients} /> */}
+              <button
+                onClick={(event) => {
+                  createRecipeFromApi();
+                  event.preventDefault();
+                }}
+              >
+                Submit My Recipe
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   );
